215e6bb6b0d94dafdfee0c17e71a966aafc99b10
NikolaJackova <nikol.jac@seznam.cz>
2021-03-18 20:18:28 +0100

    Parser structure

---
+++ ITEJA_ICSHP_Jačková_Nikola/LanguageLibrary/Parser/Expressions/Multiply.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageLibrary.Parser.Expressions
+{
+    class Multiply : BinaryExpression
+    {
+    }
+}
