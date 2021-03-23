ca889c8153714ed409d7a376bc3e10a50fcd141c
NikolaJackova <nikol.jac@seznam.cz>
2021-03-23 16:21:08 +0100

    Added custom exceptions

---
+++ ITEJA_ICSHP_Jačková_Nikola/LanguageLibrary/Exceptions/LanguageException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageLibrary.Exceptions
+{
+    public class LanguageException : Exception
+    {
+        public LanguageException(string message) : base(message) { }
+    }
+}
---
+++ ITEJA_ICSHP_Jačková_Nikola/LanguageLibrary/Exceptions/ParserException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageLibrary.Exceptions
+{
+    public class ParserException : LanguageException
+    {
+        public ParserException(string message) : base(message) { }
+    }
+}
